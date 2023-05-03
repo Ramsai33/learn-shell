8ddc3b8a9f54178f0d2ae321de7c88aecc8abbfe
Ramsai35 <mudurukollaramsai335@gmail.com>
2023-05-03 10:00:17 +0530

    Frontend script

---
+++ catalogue.sh
@@ -0,0 +1,32 @@
+script_location=$(pwd)
+
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+
+yum install nodejs -y
+
+useradd roboshop
+
+mkdir /app
+
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
+
+cd /app
+
+unzip /tmp/catalogue.zip
+
+npm install
+
+cp ${script_location}/files/nodejs.systemd /etc/systemd/system/catalogue.service
+
+systemctl daemon-reload
+
+systemctl enable catalogue
+systemctl start catalogue
+
+cp ${script_location}/files/schemaload.repo /etc/yum.repos.d/mongo.repo
+
+yum install mongodb-org-shell -y
+
+mongo --host 172.31.85.198 </app/schema/catalogue.js
+
+
