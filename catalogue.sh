fd8aca36f172f230ae00e489a685f5d83a4e61ce
Ramsai35 <mudurukollaramsai335@gmail.com>
2023-05-07 15:04:12 +0530

    Catalogue

--- catalogue.sh
+++ catalogue.sh
@@ -3,7 +3,7 @@ LOG=/tmp/roboshop.log
 
 status_check() {
   if [ $? -eq 0 ]; then
-    echo -e "\e[31m Success \e[0m"
+    echo -e "\e[32m Success \e[0m"
   else
     echo -e "\e[31m Failure \e[0m"
     echo "Refer error log file for more information, Log- ${LOG}"
