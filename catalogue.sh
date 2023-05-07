1d640e5fc2da4ac02955024127cd82b837f7f032
Ramsai35 <mudurukollaramsai335@gmail.com>
2023-05-07 15:24:20 +0530

    Catalogue

--- catalogue.sh
+++ catalogue.sh
@@ -1,15 +1,4 @@
-script_location=$(pwd)
-LOG=/tmp/roboshop.log
-
-status_check() {
-  if [ $? -eq 0 ]; then
-    echo -e "\e[32m Success \e[0m"
-  else
-    echo -e "\e[31m Failure \e[0m"
-    echo "Refer error log file for more information, Log- ${LOG}"
-  exit
-  fi
-}
+source common.sh
 
 echo -e "\e[33mDownloadingnodejsrepo\e[0m"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${LOG}
