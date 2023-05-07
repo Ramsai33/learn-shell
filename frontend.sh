7811e36f16dd6a88b9abb21f247c22d6a555d2e7
Ramsai35 <mudurukollaramsai335@gmail.com>
2023-05-07 16:03:02 +0530

    MongoDB

--- frontend.sh
+++ frontend.sh
@@ -1,13 +1,26 @@
-script_location=$(pwd)
+source common.sh
 
-yum install nginx -y
-systemctl enable nginx
-systemctl start nginx
-rm -rf /usr/share/nginx/html/*
+print_head "Installing Nginx"
+yum install nginx -y &>>${LOG}
+status_check
 
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+print_head "Starting Service"
+systemctl enable nginx &>>${LOG}
+systemctl start nginx &>>${LOG}
+status_check
 
-cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
 
-cp ${script_location}/files/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
+rm -rf /usr/share/nginx/html/* &>>${LOG}
+
+print_head "Downloading web content"
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${LOG}
+status_check
+
+print_head "Extracting web content"
+cd /usr/share/nginx/html &>>${LOG}
+unzip /tmp/frontend.zip &>>${LOG}
+status_check
+
+print_head "Configuring Reverse Proxy"
+cp ${script_location}/files/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${LOG}
+status_check
--- mongod.sh
+++ mongod.sh
@@ -1,9 +1,19 @@
-script_location=$(pwd)
-cp ${script_location}/files/mongod.repo /etc/yum.repos.d/mongo.repo
-yum install mongodb-org -y
-systemctl enable mongod
-systemctl start mongod
 
-sed -i -e 's/127.0.0.1/0.0.0.0/gi' /etc/mongod.conf
+cp ${script_location}/files/mongod.repo /etc/yum.repos.d/mongo.repo &>>${LOG}
 
-systemctl restart mongod
+print_head "Install MongoDB"
+yum install mongodb-org -y &>>${LOG}
+Status_check
+
+print_head "Stat Service"
+systemctl enable mongod &>>${LOG}
+systemctl start mongod &>>${LOG}
+Status_check
+
+print_head "Changing Port Details "
+sed -i -e 's/127.0.0.1/0.0.0.0/gi' /etc/mongod.conf &>>${LOG}
+Status_check
+
+print_head "Restart MongoDB "
+systemctl restart mongod &>>${LOG}
+Status_check
