c23bf4379f9d1cc1518baf019bfd75c80dea762d
Ramsai35 <mudurukollaramsai335@gmail.com>
2023-05-08 07:30:29 +0530

    User

--- mongod.sh
+++ mongod.sh
@@ -4,17 +4,17 @@ cp ${script_location}/files/mongod.repo /etc/yum.repos.d/mongo.repo &>>${LOG}
 
 print_head "Install MongoDB"
 yum install mongodb-org -y &>>${LOG}
-Status_check
+status_check
 
 print_head "Stat Service"
 systemctl enable mongod &>>${LOG}
 systemctl start mongod &>>${LOG}
-Status_check
+status_check
 
 print_head "Changing Port Details "
 sed -i -e 's/127.0.0.1/0.0.0.0/gi' /etc/mongod.conf &>>${LOG}
-Status_check
+status_check
 
 print_head "Restart MongoDB"
 systemctl restart mongod &>>${LOG}
-Status_check
+status_check
